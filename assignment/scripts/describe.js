ecff1e2bb35ebbbfbaefa240c2570b7d7175ba06
Mahad Hassan <mahadh578@gmail.com>
2024-03-03 18:38:24 -0600

    filled out assignemtns with open ended responses on js

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,10 +26,10 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We make a variable called "name" and make the value 'Dane'
+// Then an if,else statement is created using another variable 'Mary'
+// The if,else statement says "if the name is 'Mary', then console.log will show 'Hi, Mary'. If not, then console.log will make 'How do you do' appear instead"
+// since the name is set to 'Dane' it would show console.log('How do you do?')
 
 //CODE
 /*
@@ -45,10 +45,11 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We create a variable 'secret' and assign no value to it
+// then we create a variable 'code' and let it be equal to 123
+// an if statement is created stating if the code is 123 then the value of secret will be "super" but then, the value of code was adjusted to code times 2 which is now 246
+// another if statement is made and states if code is greater than 250 the value of secret will be "duper"
+// console.log(secret) will appear as 'super'
 
 //CODE
 /*
@@ -70,10 +71,13 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// First we gave our variables values and put them in a compound conditional statement
+// It states that if isStudent is true and the zip is greater than 00000, console.log(`You're a student on the West Coast!)
+// However at the same time if isStudent is false OR the age is younger than 30, console.log('What are your hobbies?'); [Which isn't the case]
+//Lastly if isStudent is true which in this case it is console.log('Welcome to Prime!'); will appear
+//Even though the last statement is true, the first correct statement will be the one to appear, so   console.log(`You're a student on the West Coast!`); will show
+
+
 
 //CODE
 /*
@@ -129,7 +133,7 @@ if (number >= 2) {
 // colorTwo is set to 'red', and mix is set to true. We check if mix is true 
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
-/*
+/* FIX - It asks to set colorOne and colorTwo to purple so it needs to include a variable for purple, I believe you have to do colorOne + colorTwo = purple. Or possibly create another variable called colorThree and have it set to purple.
 let colorOne = 'red';
 let colorTwo = 'blue';
 let mix = true;
@@ -143,7 +147,7 @@ if (mix === true) {
 // We check if temp is higher than 39 and if time is greater or equal to  4 
 // -- they are so we console.log 'throw away the food!'
 
-/*
+/* Time cannot be a constant it has to be a variable considering that in this problem time can be greater or equal to 4
 let temp = 40;
 const time = 4;
 
@@ -156,7 +160,7 @@ if (temp > 39 || time >= 4) {
 // We check if age is greater than or equal to minAge 
 // -- it is, so we console.log 'enter'.
 
-/*
+/* console.log will show 'no entry') instead of 'enter' due to what the conditonal is stating. I would switch the names in the logs to make this properly function.
 let age = 21;
 const minAge = 21;
 
